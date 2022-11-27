888c74e5b1f3c522ad3c4d3adbe1e8fc3604671d
Birkan Dogan <101419153+birkan-dogan@users.noreply.github.com>
2022-11-27 17:41:47 +0300

    update

--- codewars/modernOperators.js
+++ codewars/modernOperators.js
@@ -101,3 +101,77 @@ printGoals(game.score[0], ...game.scored);
 console.log(
   `${(team1 < team2 && "Bayern") || "Dortmund"} is more likely to win`
 );
+
+// challenge 2
+
+
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+
+for (const [index, player] of game.scored.entries()) {
+  console.log(`Goal ${index + 1}: ${player}`);
+}
+
+/*
+Goal 1: Lewandowski
+Goal 2: Gnarby
+Goal 3: Lewandowski
+Goal 4: Hummels
+*/
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember
+
+const odds = Object.values(game.odds);
+let sumOfOdds = 0;
+
+for (const x of odds) {
+  sumOfOdds += x;
+}
+
+const average = sumOfOdds / odds.length;
+console.log(average.toFixed(2)); // 3.69
+
+/*
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw").
+*/
+
+const allOdd = Object.entries(game.odds);
+console.log(allOdd);
+
+for (const [team, odd] of allOdd) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd} `);
+}
+
+/*
+
+4. Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this
+
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+
+*/
+
+const scorers = {};
+const keys = Object.keys(scorers);
+
+for (const player of game.scored) {
+  if (!keys.includes(player)) {
+    scorers[player] = 1;
+    keys.push(player);
+  } else {
+    scorers[player] += 1;
+  }
+}
+console.log(scorers);
+
+// {Lewandowski: 2, Gnarby: 1, Hummels: 1}
+
